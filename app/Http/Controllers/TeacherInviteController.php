80976a23ab2ad04e802be5b03d0522fccd158441
stevenv <stevenv.w.w@gmail.com>
2021-10-11 22:33:09 +0800

    邀请老师

---
+++ app/Http/Controllers/TeacherInviteController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class TeacherInviteController extends Controller
+{
+    public function invite() {
+
+    }
+}
---
+++ app/Http/Services/TeacherInviteService.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http\Services;
+
+
+class TeacherInviteService
+{
+    public function invite(int $teacherId, string $email): array {
+
+    }
+}
