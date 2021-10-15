87752ad78244f91d67b3e5b84da116d57d8b1670
stevenv <stevenv.w.w@gmail.com>
2021-10-15 09:41:47 +0800

    update teacher_invite

--- database/migrations/2021_10_11_214035_create_teacher_invite.php
+++ database/migrations/2021_10_11_214035_create_teacher_invite.php
@@ -13,14 +13,13 @@ class CreateTeacherInvite extends Migration
      */
     public function up()
     {
-        if (!Schema::hasTable('teacher_invite')) {
+        if (!Schema::hasTable('teacher_follow')) {
             Schema::create('teacher_invite', function (Blueprint $table) {
                 $table->increments('id');
-                $table->integer("teacher_id")->comment("老师id");
+                $table->integer("teacher_id")->comment("老师id")->index();
                 $table->string("email", 32)->nullable(false)->default("")->comment("邮箱");
                 $table->tinyInteger("status")->nullable(false)->default(0)->comment("状态:0待接受,1邀请成功,2邀请过期");
                 $table->tinyInteger("is_sent")->nullable(false)->default(0)->comment("是否发送邮件: 1已发送 0未发送");
-                $table->index("teacher_id", "idx_teacher_id");
                 $table->timestamps();
             });
         }
