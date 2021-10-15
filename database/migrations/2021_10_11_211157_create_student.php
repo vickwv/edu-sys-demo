bb1a3524640441d9ea91bc74e16d69dcabe81ea9
stevenv <stevenv.w.w@gmail.com>
2021-10-15 09:30:17 +0800

    update students

--- database/migrations/2021_10_11_211157_create_student.php
+++ database/migrations/2021_10_11_211157_create_student.php
@@ -17,14 +17,13 @@ class CreateStudent extends Migration
             Schema::create('student', function (Blueprint $table) {
                 $table->increments('id');
                 $table->integer("school_id", false)->nullable(false)->comment("学校id");
-                $table->string("email", 32)->nullable(false)->default("")->comment("邮箱");
+                $table->string("email", 32)->nullable(false)->unique()->default("")->comment("邮箱");
                 $table->string("name", 20)->nullable(false)->default("")->comment("名称");
                 $table->string("password", 100)->nullable(false)->default("")->comment("密码");
                 $table->char("birthday", 10)->default("")->comment("生日");
                 $table->tinyInteger("sex")->default(0)->comment("性别:0未知1男2女");
                 $table->tinyInteger("age")->default(0)->comment("年龄");
                 $table->tinyInteger("status")->comment("状态: 1正常,0禁用");
-                $table->unique("email", "idx_email");
                 $table->index("school_id", "idx_school_id");
                 $table->timestamps();
             });
