1ec1fdfc9e9019294990cc2b5e94e777ff49e35b
stevenv <stevenv.w.w@gmail.com>
2021-10-15 09:18:40 +0800

    user errorlog

---
+++ config/logging.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'default' => env('LOG_CHANNEL', 'stack'),
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['single'],
+        ],
+        'single' => [
+            'driver' => 'errorlog',
+            'level' => 'debug',
+        ],
+    ]
+];
